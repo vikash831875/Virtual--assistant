e33e142b478daaf8fd0319143deee284f9e9984d
vikash831875 <vikashkumarpatel638@gmail.com>
2025-12-11 16:42:20 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -34,8 +34,8 @@ export const signUp = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // LOCALHOST FIX
-      sameSite: "lax",
+      secure: true, // LOCALHOST FIX
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -67,8 +67,8 @@ export const Login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // LOCALHOST FIX
-      sameSite: "lax",
+      secure: true, // LOCALHOST FIX
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
