da12a5c037913ea1177d7d1d8aea46e7e282854e
vikash831875 <vikashkumarpatel638@gmail.com>
2025-12-11 16:39:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-frontend-kt81.onrender.com",
     credentials: true
 }));
 
