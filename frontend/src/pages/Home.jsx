9041d90648bd96e0f5f3efd8ca47820f5ec2c68f
vikash831875 <vikashkumarpatel638@gmail.com>
2025-12-11 16:29:57 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import aiImg from "../assets/ai.gif";
 import userImg from "../assets/user.gif";
